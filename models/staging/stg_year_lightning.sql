c519658fbd767866e9035a319e746a49c603aa96
kristupaskan@gmail.com <kristupaskan@gmail.com>
2024-11-16 20:32:30 +0000

    added new queries

--- models/staging/stg_year_lightning.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-    EXTRACT(YEAR FROM date) AS year,
-    SUM(number_of_strikes) AS total_strikes
-FROM 
-    `bigquery-public-data.noaa_lightning.lightning_strikes`
-GROUP BY 
-    EXTRACT(YEAR FROM date)
-ORDER BY 
-    year
